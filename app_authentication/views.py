56ed57cd5ddf116929c21454e20a7aa26cd6ac50
Fan Wang <nimadorostkar97@gmail.com>
2022-03-30 01:59:03 +0430

    views

--- app_authentication/views.py
+++ app_authentication/views.py
@@ -1,3 +1,54 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import User
+from django.forms.utils import ErrorList
+from django.http import HttpResponse, HttpResponseRedirect, request
+from .forms import LoginForm, SignUpForm
+from django.contrib import messages
 
-# Create your views here.
+
+
+
+
+
+#------------------------------------------------------------------------------
+def login(request):
+    if request.method == "POST":
+        user = authenticate( username=request.POST['username'], password=request.POST['password'] )
+        login(request, user)
+        return redirect("/")
+    return render(request, "account/login.html")
+
+
+
+
+
+
+
+
+'''
+#------------------------------------------------------------------------------
+def register_user(request):
+    msg     = None
+    success = False
+
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get("username")
+            raw_password = form.cleaned_data.get("password1")
+            user = authenticate(username=username, password=raw_password)
+
+            msg     = 'کاربر ایجاد شد - <a href="/login">ورود</a>.'
+            success = True
+
+            #return redirect("/login/")
+
+        else:
+            msg = 'اطلاعات فرم معتبر نیست'
+    else:
+        form = SignUpForm()
+
+    return render(request, "accounts/register.html", {"form": form, "msg" : msg, "success" : success })
+'''
