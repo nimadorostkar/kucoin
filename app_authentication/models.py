73468b70699dc6a9f60d8cdc1ab559c4ce977bc8
Fan Wang <nimadorostkar97@gmail.com>
2022-03-31 05:37:01 +0430

    Exchange model

--- app_authentication/models.py
+++ app_authentication/models.py
@@ -49,6 +49,30 @@ class Profile(models.Model):
 
 
 
+#------------------------------------------------------------------------------
+class Exchange(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="کاربر")
+    name = models.CharField(max_length=80, verbose_name="نام")
+    description = models.TextField(verbose_name="توضیحات")
+    api_key = models.CharField(max_length=100)
+    api_secret = models.CharField(max_length=100)
+    api_passphrase = models.CharField(max_length=100)
+
+    def get_absolute_url(self):
+        return reverse('exchange',args=[self.id])
+
+    def __str__(self):
+        return str(self.name)
+
+    class Meta:
+      verbose_name = "صرافی"
+      verbose_name_plural = "صرافی ها"
+
+
+
+
+
+
 
 
 
